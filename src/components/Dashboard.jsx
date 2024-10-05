b29897636e0d921f020a7d8832614e44aae6737f
Apolo151 <abdallah.ibrahim5115@gmail.com>
2024-10-05 09:56:12 +0300

    final

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,205 +1,196 @@
-// src/components/Dashboard.js
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Line } from 'react-chartjs-2';
-import { Chart, registerables } from 'chart.js';
-import { getNasaPowerData } from '../services/nasaDataService';
+import { getNasaPowerData } from '../services/nasaDataService'; // Assuming you have an API function to fetch data
+import { TextField, Button, Box, Typography } from '@mui/material';
+import { Chart, LinearScale, CategoryScale, LineElement, PointElement, Tooltip, Legend, Title } from 'chart.js';
 
-// Register all necessary components in Chart.js
-Chart.register(...registerables);
+// Register required components from Chart.js
+Chart.register(LinearScale, CategoryScale, LineElement, PointElement, Tooltip, Legend, Title);
 
 const Dashboard = () => {
+    const getDefaultStartDate = () => {
+        const date = new Date();
+        date.setDate(date.getDate() - 7);
+        return date.toISOString().split('T')[0];
+    };
+
+    const getDefaultEndDate = () => {
+        const date = new Date();
+        return date.toISOString().split('T')[0];
+    };
+
     const [dates, setDates] = useState([]);
     const [solarRadiation, setSolarRadiation] = useState([]);
     const [temperature, setTemperature] = useState([]);
-    const [precipitation, setPrecipitation] = useState([]);
     const [surfacePressure, setSurfacePressure] = useState([]);
     const [windSpeed, setWindSpeed] = useState([]);
     const [soilMoisture, setSoilMoisture] = useState([]);
     const [surfaceSoilMoisture, setSurfaceSoilMoisture] = useState([]);
+    const [startDate, setStartDate] = useState(getDefaultStartDate());
+    const [endDate, setEndDate] = useState(getDefaultEndDate());
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const data = await getNasaPowerData(25, 25); // Example coordinates (latitude, longitude)
-                const solarData = data.properties.parameter.ALLSKY_SFC_SW_DWN;
-                const tempData = data.properties.parameter.T2M;
-                const precipData = data.properties.parameter.PRECTOT;
-                const surfacePressureData = data.properties.parameter.PS;
-                const windSpeedData = data.properties.parameter.WS2M;
-                const soilMoistureData = data.properties.parameter.GWETPROF;
-                const surfaceSoilMoistureData = data.properties.parameter.GWETTOP;
-
-                // Set the data for the charts
-                setDates(Object.keys(solarData)); // The dates (x-axis)
-                setSolarRadiation(Object.values(solarData)); // Solar radiation data (y-axis)
-                setTemperature(Object.values(tempData)); // Temperature data (y-axis)
-                setPrecipitation(Object.values(precipData)); // Precipitation data (y-axis)
-                setSurfacePressure(Object.values(surfacePressureData)); // Surface pressure data (y-axis)
-                setWindSpeed(Object.values(windSpeedData)); // Wind speed data (y-axis)
-                setSoilMoisture(Object.values(soilMoistureData)); // Soil moisture data (y-axis)
-                setSurfaceSoilMoisture(Object.values(surfaceSoilMoistureData)); // Surface soil moisture data (y-axis)
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
-        };
-
         fetchData();
-    }, []);
+    }, [startDate, endDate]);
 
-    // Data and options for Solar Radiation, Temperature, Precipitation chart
+    const fetchData = async () => {
+        try {
+            // remove - from date
+            const sDate = startDate.replace(/-/g, '');
+            const eDate = endDate.replace(/-/g, '');
+            const data = await getNasaPowerData(25, 25, sDate, eDate); // Example coordinates (latitude, longitude)
+            const solarData = data.properties.parameter.ALLSKY_SFC_SW_DWN;
+            const tempData = data.properties.parameter.T2M;
+            const surfacePressureData = data.properties.parameter.PS;
+            const windSpeedData = data.properties.parameter.WS2M;
+            const soilMoistureData = data.properties.parameter.GWETPROF;
+            const surfaceSoilMoistureData = data.properties.parameter.GWETTOP;
+
+            // Set the data for the charts
+            setDates(Object.keys(solarData)); // The dates (x-axis)
+            setSolarRadiation(Object.values(solarData)); // Solar radiation data (y-axis)
+            setTemperature(Object.values(tempData)); // Temperature data (y-axis)
+            setSurfacePressure(Object.values(surfacePressureData)); // Surface pressure data (y-axis)
+            setWindSpeed(Object.values(windSpeedData)); // Wind speed data (y-axis)
+            setSoilMoisture(Object.values(soilMoistureData)); // Soil moisture data (y-axis)
+            setSurfaceSoilMoisture(Object.values(surfaceSoilMoistureData)); // Surface soil moisture data (y-axis)
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
+
+    const handleDateChange = (setter) => (event) => {
+        setter(event.target.value);
+    };
+
     const climateChartData = {
         labels: dates,
         datasets: [
             {
-                label: 'Solar Radiation (W/m²)',
+                label: 'Solar Radiation',
                 data: solarRadiation,
-                borderColor: 'rgba(255, 206, 86, 1)', // Yellow
-                backgroundColor: 'rgba(255, 206, 86, 0.5)',
-                fill: true,
+                borderColor: 'rgba(255, 99, 132, 1)',
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
                 yAxisID: 'y1',
             },
             {
-                label: 'Temperature (°C)',
+                label: 'Temperature',
                 data: temperature,
-                borderColor: 'rgba(75, 192, 192, 1)', // Teal
-                backgroundColor: 'rgba(75, 192, 192, 0.5)',
-                fill: false,
+                borderColor: 'rgba(54, 162, 235, 1)',
+                backgroundColor: 'rgba(54, 162, 235, 0.2)',
                 yAxisID: 'y2',
             },
             {
-                label: 'Precipitation (mm)',
-                data: precipitation,
-                borderColor: 'rgba(153, 102, 255, 1)', // Purple
-                backgroundColor: 'rgba(153, 102, 255, 0.5)',
-                fill: false,
+                label: 'Surface Pressure',
+                data: surfacePressure,
+                borderColor: 'rgba(75, 192, 192, 1)',
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
                 yAxisID: 'y3',
+            },
+            {
+                label: 'Wind Speed',
+                data: windSpeed,
+                borderColor: 'rgba(153, 102, 255, 1)',
+                backgroundColor: 'rgba(153, 102, 255, 0.2)',
+                yAxisID: 'y4',
+            },
+            {
+                label: 'Soil Moisture',
+                data: soilMoisture,
+                borderColor: 'rgba(255, 159, 64, 1)',
+                backgroundColor: 'rgba(255, 159, 64, 0.2)',
+                yAxisID: 'y5',
+            },
+            {
+                label: 'Surface Soil Moisture',
+                data: surfaceSoilMoisture,
+                borderColor: 'rgba(255, 206, 86, 1)',
+                backgroundColor: 'rgba(255, 206, 86, 0.2)',
+                yAxisID: 'y6',
             },
         ],
     };
 
-    const climateChartOptions = {
+    const options = {
         scales: {
             y1: {
                 type: 'linear',
                 position: 'left',
-                title: {
-                    display: true,
-                    text: 'Solar Radiation (W/m²)',
-                },
+                title: { display: true, text: 'Solar Radiation' },
+                grid: { drawOnChartArea: false },
             },
             y2: {
                 type: 'linear',
                 position: 'right',
-                title: {
-                    display: true,
-                    text: 'Temperature (°C)',
-                },
-                grid: {
-                    drawOnChartArea: false, // Removes the grid lines for this axis
-                },
+                title: { display: true, text: 'Temperature (°C)' },
+                grid: { drawOnChartArea: false },
             },
             y3: {
                 type: 'linear',
                 position: 'right',
-                title: {
-                    display: true,
-                    text: 'Precipitation (mm)',
-                },
-                grid: {
-                    drawOnChartArea: false, // No grid lines for this axis either
-                },
+                title: { display: true, text: 'Surface Pressure (hPa)' },
+                grid: { drawOnChartArea: false },
             },
+            y4: {
+                type: 'linear',
+                position: 'right',
+                title: { display: true, text: 'Wind Speed (m/s)' },
+                grid: { drawOnChartArea: false },
+            },
+            y5: {
+                type: 'linear',
+                position: 'right',
+                title: { display: true, text: 'Soil Moisture (%)' },
+                grid: { drawOnChartArea: false },
+            },
+            y6: {
+                type: 'linear',
+                position: 'right',
+                title: { display: true, text: 'Surface Soil Moisture (%)' },
+                grid: { drawOnChartArea: false },
+            }
         },
-    };
-
-    // Data and options for Surface Pressure and Wind Speed chart
-    const pressureWindChartData = {
-        labels: dates,
-        datasets: [
-            {
-                label: 'Surface Pressure (Pa)',
-                data: surfacePressure,
-                borderColor: 'rgba(255, 99, 132, 1)', // Red
-                backgroundColor: 'rgba(255, 99, 132, 0.5)',
-                fill: true,
+        plugins: {
+            legend: {
+                display: true,
             },
-            {
-                label: 'Wind Speed (m/s)',
-                data: windSpeed,
-                borderColor: 'rgba(54, 162, 235, 1)', // Blue
-                backgroundColor: 'rgba(54, 162, 235, 0.5)',
-                fill: true,
-            },
-        ],
-    };
-
-    const pressureWindChartOptions = {
-        scales: {
-            y: {
-                beginAtZero: true,
-                title: {
-                    display: true,
-                    text: 'Pressure (Pa) & Wind Speed (m/s)',
-                },
-            },
-        },
-    };
-
-    // Data and options for Soil Moisture chart
-    const soilMoistureChartData = {
-        labels: dates,
-        datasets: [
-            {
-                label: 'Soil Moisture (m³/m³)',
-                data: soilMoisture,
-                borderColor: 'rgba(255, 159, 64, 1)', // Orange
-                backgroundColor: 'rgba(255, 159, 64, 0.5)',
-                fill: true,
-            },
-            {
-                label: 'Surface Soil Moisture (m³/m³)',
-                data: surfaceSoilMoisture,
-                borderColor: 'rgba(75, 192, 192, 1)', // Teal
-                backgroundColor: 'rgba(75, 192, 192, 0.5)',
-                fill: true,
-            },
-        ],
-    };
-
-    const soilMoistureChartOptions = {
-        scales: {
-            y: {
-                beginAtZero: true,
-                title: {
-                    display: true,
-                    text: 'Soil Moisture (m³/m³)',
-                },
+            title: {
+                display: true,
+                text: 'Climate Data Dashboard',
             },
         },
     };
 
     return (
-        <div>
-            <h1>Agricultural Dashboard</h1>
-            
-            {/* Climate Chart */}
-            <h2>Climate Data</h2>
-            <div>
-                <Line data={climateChartData} options={climateChartOptions} />
-            </div>
-            
-            {/* Surface Pressure and Wind Speed Chart */}
-            <h2>Surface Pressure and Wind Speed</h2>
-            <div>
-                <Line data={pressureWindChartData} options={pressureWindChartOptions} />
-            </div>
-
-            {/* Soil Moisture Chart */}
-            <h2>Soil Moisture</h2>
-            <div>
-                <Line data={soilMoistureChartData} options={soilMoistureChartOptions} />
-            </div>
-        </div>
+        <Box sx={{ padding: 4 }}>
+            <Typography variant="h4" align="center" gutterBottom>
+                Climate Data Dashboard
+            </Typography>
+            <Box display="flex" justifyContent="center" mb={4} gap={2}>
+                <TextField
+                    label="Start Date"
+                    type="date"
+                    value={startDate}
+                    onChange={handleDateChange(setStartDate)}
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                />
+                <TextField
+                    label="End Date"
+                    type="date"
+                    value={endDate}
+                    onChange={handleDateChange(setEndDate)}
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                />
+                <Button variant="contained" color="primary" onClick={fetchData}>
+                    Update
+                </Button>
+            </Box>
+            <Line data={climateChartData} options={options} />
+        </Box>
     );
 };
 
--- src/services/nasaDataService.js
+++ src/services/nasaDataService.js
@@ -3,13 +3,13 @@ import axios from 'axios';
 
 const NASA_API_URL = 'https://power.larc.nasa.gov/api/temporal/daily/point';
 
-export const getNasaPowerData = async (lat, lon) => {
+export const getNasaPowerData = async (lat, lon, startDate, endDate) => {
     try {
         const response = await axios.get(NASA_API_URL, {
             params: {
                 parameters: 'ALLSKY_SFC_SW_DWN,T2M,PRECTOT,PS,GWETTOP,GWETPROF,WS2M',
-                start: '20240926',
-                end: '20241005',
+                start: startDate,
+                end: endDate,
                 community: 'ag',
                 longitude: lon,
                 latitude: lat,
